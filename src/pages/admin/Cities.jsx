7207b476474e267f2c069a704f27180fc2807bd0
Aashu <aashutoshk625@gmail.com>
2026-07-19 22:42:38 +0530

    fix: remove unnecessary placeholder text from state input field in Cities admin page

--- src/pages/admin/Cities.jsx
+++ src/pages/admin/Cities.jsx
@@ -1403,7 +1403,8 @@ const Cities = () => {
                     type="text"
                     required
                     placeholder="e.g., Bihar"
-                    value={formData.state}
+                    value={formData.state} delte krte hai 
+                    
                     onChange={(e) => setFormData({ ...formData, state: e.target.value })}
                     className="w-full px-4 py-2.5 rounded-xl border border-gray-250 dark:border-white/10 bg-transparent text-sm focus:outline-none focus:ring-1 focus:ring-ozo-red dark:text-white font-semibold"
                   />
